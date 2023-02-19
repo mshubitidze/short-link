665e133be32d3cce9093e5af8ed3cb56033b8272
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-02-19 11:25:59 +0400

    😇

---
+++ src/pages/healthcheck.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Healthcheck = () => {
+  return (
+    <div className="flex justify-center items-center h-screen text-4xl text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-green-500 via-sky-500">
+      სანდრო, ყლეზე მაკოცე
+    </div>
+  );
+};
+
+export default Healthcheck;

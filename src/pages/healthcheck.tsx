c9fee32e1dc448e5cceafb4165fd5f7d8a7fe0b4
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-02-19 11:36:45 +0400

    ok

--- src/pages/healthcheck.tsx
+++ src/pages/healthcheck.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { api } from "../utils/api";
 
 const Healthcheck = () => {
+  const fetchLongUrl = api.shortUrlRouter.fetchLongUrl.useQuery(
+    {
+      shortUrl: "misho",
+    },
+    {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    }
+  );
   return (
-    <div className="flex justify-center items-center h-screen text-4xl text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-green-500 via-sky-500">
-      სანდრო, ყლეზე მაკოცე
+    <div className="flex justify-center flex-col gap-4 items-center h-screen text-4xl text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-green-500 via-sky-500">
+      <p>სანდრო, ყლეზე მაკოცე</p>
+      {fetchLongUrl?.data ? <div>OK</div> : <div>Loading...</div>}
     </div>
   );
 };
